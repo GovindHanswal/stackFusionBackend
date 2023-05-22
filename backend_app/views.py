0e8aca76bab5931d591cd4f43b5e67c02625a53a
Govind Hanswal <hanswalgovind@gmail.com>
2023-05-22 17:13:33 +0530

    Update views.py

--- backend_app/views.py
+++ backend_app/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 import json
 from .models import UserForm
-
-
+from django.core.mail import send_mail
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+import re
 # Create your views here.
 
 @csrf_exempt
@@ -16,9 +17,18 @@ def submit_form(request):
         phone = data.get('phone')
 
         # Perform form validation and other logic here
+        phone_regex = re.compile(r'^\d{10}$')
+        if not phone_regex.match(phone):
+            return JsonResponse({'message': 'Invalid phone number'}, status=400)
 
         form = UserForm(name=name, dob=dob, email=email, phone=phone)
         form.save()
+        # send_email
+        subject="Here is the confirmation mail"
+        message="Thank you for joining us"
+        from_email=settings.EMAIL_HOST_USER
+        to_email=[email]
+        send_mail(subject,message,from_email,to_email)
 
         return JsonResponse({'message': 'Form submitted successfully'})
     else:
